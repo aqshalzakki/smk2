3c030b2c47cb4a628d58264bddebb9d31487a4f8
Aqshalzakki <kecoatempur2@gmail.com>
2019-08-28 19:04:48 +0700

    mengedit bagian topbar

--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -30,7 +30,7 @@ $nama_level = strtolower($this->db->get_where('level', ['id_level' => $id_level]
         <li class="nav-item dropdown no-arrow">
           <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $this->session->userdata('user')['nama']; ?></span>
-            <i class='fas fa-fw fa-user'></i>
+            <img src="<?= base_url('vendor/img/' . $user['gambar']); ?>" class="img-profile rounded-circle" alt="">
           </a>
           <!-- Dropdown - User Information -->
           <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
