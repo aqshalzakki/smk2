4d8d1b1a66c0dc741e76358daff3415a46393153
Aqshalzakki <kecoatempur2@gmail.com>
2019-08-28 19:11:25 +0700

    menambahkan margin kiri pada tiap2 input

--- application/views/smk2/edit-profile.php
+++ application/views/smk2/edit-profile.php
@@ -6,17 +6,17 @@
                 <div class="form-group">
                     <label for="nama">Nama</label>
                     <input type="text" class="form-control <?= is_invalid('nama'); ?>" name="nama" id="nama" value="<?= $user['nama_pegawai']; ?>">
-                    <?= form_error('nama', '<small class="text-danger">', '</small>'); ?>
+                    <?= form_error('nama', '<small class="ml-2 text-danger">', '</small>'); ?>
                 </div>
                 <div class="form-group">
                     <label for="nip">NIP</label>
                     <input type="text" class="form-control <?= is_invalid('nip'); ?>" name="nip" id="nip" value="<?= $user['nip']; ?>">
-                    <?= form_error('nip', '<small class="text-danger">', '</small>'); ?>
+                    <?= form_error('nip', '<small class="ml-2 text-danger">', '</small>'); ?>
                 </div>
                 <div class="form-group">
                     <label for="alamat">Alamat</label>
                     <input type="text" class="form-control <?= is_invalid('alamat'); ?>" name="alamat" id="alamat" value="<?= $user['alamat']; ?>">
-                    <?= form_error('alamat', '<small class="text-danger">', '</small>'); ?>
+                    <?= form_error('alamat', '<small class="ml-2 text-danger">', '</small>'); ?>
                 </div>
                 <div class="form-group">
                     <div class="custom-file my-2">
