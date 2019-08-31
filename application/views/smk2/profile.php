e4dca501c2c45fbdfa8f11510058005d59cc7631
ihsanaryandi <jfrosty426@gmail.com>
2019-08-31 12:52:35 +0700

    melakukan perubahan card di halaman profile

--- application/views/smk2/profile.php
+++ application/views/smk2/profile.php
@@ -1,16 +1,15 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-12">
         <h1 class="mb-2">Profile saya</h1>
         <?= $this->session->flashdata('message'); ?>
 
             <div class="card my-4">
                 <div class="row no-gutters">
-                    <div class="col-md-4">
-                        <img src="<?= base_url('vendor/img/' . $user['gambar']); ?>" class="card-img" alt="...">
+                    <div class="col-md-5">
+                        <img src="<?= base_url('vendor/img/' . $user['gambar']); ?>" class="card-img" style="height: 100%; width: 100%;">
                     </div>
-                    <div class="col-md-8">
                     <div class="card-body">
                         <?php if ($user['id_level'] == 1) : ?>
                             <h5 class="card-title"><?= $user['nama_admin']; ?></h5>
@@ -23,7 +22,6 @@
                         <?php endif; ?>
                             <p class="card-text">Alamat : <?= $user['alamat']; ?></p>
                             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
                     </div>
                 </div>
             </div>
